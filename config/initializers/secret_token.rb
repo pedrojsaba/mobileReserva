bef8126eab998bbbd244acd14d346b554f0eee33
PedroBJS <pedro.jancachagua@gmail.com>
2012-01-13 15:06:05 -0500

    iniciando proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileReserva::Application.config.secret_token = '209f7e9789b321947778b9d1dec234120ea15a7bfc446d69359349b25607b8885c1bd38cd0dd93393f274b333bc39516efe2ed23f8b3c3f15529a57ff3dd6fb6'
